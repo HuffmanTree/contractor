9084df39161089ffbba5df6fb32c6c17c7c1c4c1
François Guérin <francoisguerin1@hotmail.fr>
2024-06-29 13:02:11 +0200

    Add `getBalance`

--- src/__tests__/services/blockchain.service.test.ts
+++ src/__tests__/services/blockchain.service.test.ts
@@ -1,4 +1,8 @@
+import { RpcClient } from "@taquito/rpc";
+import { BigNumber } from "bignumber.js";
 import { expect } from "chai";
+import { stub } from "sinon";
+import { Web3Eth } from "web3";
 import type { TezosProfile } from "../../models/profile.model.js";
 import { BlockchainProviderFactory, EthereumProvider, TezosProvider } from "../../services/blockchain.service.js";
 
@@ -19,5 +23,37 @@ describe("Blockchain Provider Factory", () => {
       blockchain: "tezos",
       url: "http://xtz.url",
     })).to.be.instanceOf(TezosProvider);
+  });
+});
+
+describe("Ethereum Provider", () => {
+  const provider = new EthereumProvider({
+    blockchain: "ethereum",
+    url: "http://eth.url",
+  });
+
+  it("gets the balance of an account", async () => {
+    const getBalance = stub(Web3Eth.prototype, "getBalance").resolves(30n);
+
+    expect(await provider.getBalance("address")).to.equal("30");
+    expect(getBalance.calledOnceWithExactly("address")).to.be.true;
+
+    getBalance.restore();
+  });
+});
+
+describe("Tezos Provider", () => {
+  const provider = new TezosProvider({
+    blockchain: "tezos",
+    url: "http://xrz.url",
+  });
+
+  it("gets the balance of an account", async () => {
+    const getBalance = stub(RpcClient.prototype, "getBalance").resolves(BigNumber(30));
+
+    expect(await provider.getBalance("address")).to.equal("30");
+    expect(getBalance.calledOnceWithExactly("address")).to.be.true;
+
+    getBalance.restore();
   });
 });
--- src/services/blockchain.service.ts
+++ src/services/blockchain.service.ts
@@ -1,17 +1,43 @@
+import { TezosToolkit } from "@taquito/taquito";
+import { Web3 } from "web3";
 import { isEthereumProfile, isTezosProfile, type EthereumProfile, type TezosProfile } from "../models/profile.model.js";
 
 export class BlockchainProviderFactory {
   static fromProfile(profile: EthereumProfile): EthereumProvider;
   static fromProfile(profile: TezosProfile): TezosProvider;
   static fromProfile(profile: unknown): BlockchainProvider {
-    if (isEthereumProfile(profile)) return new EthereumProvider();
-    if (isTezosProfile(profile)) return new TezosProvider();
+    if (isEthereumProfile(profile)) return new EthereumProvider(profile);
+    if (isTezosProfile(profile)) return new TezosProvider(profile);
     throw new Error("Unrecognized profile");
   }
 }
 
-interface BlockchainProvider {}
+interface BlockchainProvider {
+  getBalance(address: string): Promise<string>;
+}
 
-export class EthereumProvider implements BlockchainProvider {}
+export class EthereumProvider implements BlockchainProvider {
+  private readonly _client: Web3;
 
-export class TezosProvider implements BlockchainProvider {}
+  constructor(profile: EthereumProfile) {
+    this._client = new Web3(profile.url);
+  }
+
+  async getBalance(address: string): Promise<string> {
+    const balance = await this._client.eth.getBalance(address);
+    return balance.toString();
+  }
+}
+
+export class TezosProvider implements BlockchainProvider {
+  private readonly _client: TezosToolkit;
+
+  constructor(profile: TezosProfile) {
+    this._client = new TezosToolkit(profile.url);
+  }
+
+  async getBalance(address: string): Promise<string> {
+    const balance = await this._client.rpc.getBalance(address);
+    return balance.toString();
+  }
+}
